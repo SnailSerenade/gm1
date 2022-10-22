45d9d81015fb6adc948094375afa2c54c0b6571b
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-10-22 14:28:34 +0800

    Set Character health

--- code/Core/Character.cs
+++ code/Core/Character.cs
@@ -8,9 +8,13 @@ public partial class Character : AnimatedEntity
 	public BattleSys.BattleMember BattleMember => Components.Get<BattleSys.BattleMember>();
 	public BattleSys.BattleActor BattleActor => Components.Get<BattleSys.BattleActor>();
 
+	public virtual float MaxHealth => 100.0f;
+
 	public override void Spawn()
 	{
 		base.Spawn();
+
+		Health = MaxHealth;
 
 		Transmit = TransmitType.Always;
 	}
