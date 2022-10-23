cefbd09796eec7ebc10d7fde0b9551b4e43b0cba
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-10-23 23:40:38 +0800

    Glyph impl from BonitoBlitz

---
+++ code/UI/Glyph/Glyph.cs
@@ -0,0 +1,39 @@
+using System;
+using Sandbox;
+using Sandbox.UI;
+
+namespace gm1.SharedUI;
+
+[UseTemplate]
+public partial class Glyph : Panel
+{
+	public Image Image { get; private set; }
+	public InputGlyphSize ButtonSize { get; private set; } = InputGlyphSize.Medium;
+	public InputButton Button { get; private set; } = 0;
+	public bool HasButton => Button != 0;
+
+	public override void SetProperty( string name, string value )
+	{
+		base.SetProperty( name, value );
+
+		if ( name == "input" )
+			Button = Enum.Parse<InputButton>( value, true );
+
+		if ( name == "size" )
+			ButtonSize = Enum.Parse<InputGlyphSize>( value, true );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !HasButton )
+			return;
+		Image.Texture = Input.GetGlyph( Button, ButtonSize, GlyphStyle.Knockout );
+		if ( Image.ComputedStyle != null )
+			Image.Style.Height = Image.ComputedStyle.FontSize ?? Image.Texture.Height;
+		else
+			Image.Style.Height = Image.Texture.Height;
+		Image.Style.AspectRatio = (float)Image.Texture.Width / Image.Texture.Height;
+	}
+}
