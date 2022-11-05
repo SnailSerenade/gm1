1744419bb6a7bada25dc8ac6b0b7e4157ca85653
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-06 01:30:41 +0800

    overworld: `CameraTrigger` & component impl

---
+++ code/Overworld/CameraTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using gm1.Core;
+using Sandbox;
+using SandboxEditor;
+
+namespace gm1.Overworld;
+
+[Library( "gm1_cameratrigger" ), HammerEntity, Solid]
+[Title( "Camera Trigger" ), Category( "Gameplay" ), Icon( "place" )]
+[Description( "Camera trigger area" )]
+public partial class CameraTrigger : Entity
+{
+	[Net]
+	[Property( Title = "Camera Location Name" ), FGDType( "target_destination" )]
+	public string CameraLocationName { get; set; } = null;
+
+	private CameraLocation _cameraLocation;
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		_cameraLocation ??= All.OfType<CameraLocation>().FirstOrDefault( cfg => cfg.Name == CameraLocationName );
+
+		if ( other is Character character )
+		{
+			character.Components.Add( new SpotCameraComponent( _cameraLocation ) );
+		}
+	}
+
+	public override void EndTouch( Entity other )
+	{
+		base.EndTouch( other );
+
+		_cameraLocation ??= All.OfType<CameraLocation>().FirstOrDefault( cfg => cfg.Name == CameraLocationName );
+
+		if ( other is not Character character )
+		{
+			return;
+		}
+
+		foreach ( var component in character.Components.GetAll<SpotCameraComponent>() )
+		{
+			if ( component.CameraLocation == _cameraLocation )
+				component.Remove();
+		}
+	}
+}
---
+++ code/Overworld/SpotCameraComponent.cs
@@ -0,0 +1,29 @@
+﻿using gm1.Core;
+using Sandbox;
+
+namespace gm1.Overworld;
+
+public class SpotCameraComponent : CameraComponent
+{
+	public CameraLocation CameraLocation { get; private set; }
+
+	public SpotCameraComponent( CameraLocation cameraLocation )
+	{
+		CameraLocation = cameraLocation;
+	}
+
+	public override void PostCameraSetup( ref CameraSetup camSetup )
+	{
+		if ( Local.Pawn is not Character character ) return;
+
+		if ( CameraLocation == null ) return;
+
+		camSetup.Position = CameraLocation.Position;
+		camSetup.Rotation = CameraLocation.Rotation;
+		camSetup.FieldOfView = CameraLocation.FieldOfView;
+		camSetup.ZNear = 4f;
+		camSetup.Viewer = null;
+	}
+
+	public override void BuildInput( InputBuilder inputBuilder ) { }
+}
