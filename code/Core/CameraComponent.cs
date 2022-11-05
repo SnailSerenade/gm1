14e3ddb67773e6df407eb5472a1ac8da06aa0876
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-05 22:08:50 +0800

    core: support `CameraComponent`

---
+++ code/Core/CameraComponent.cs
@@ -0,0 +1,9 @@
+﻿using Sandbox;
+
+namespace gm1.Core;
+
+public abstract class CameraComponent : CharacterComponent
+{
+	public abstract void PostCameraSetup( ref CameraSetup camSetup );
+	public abstract void BuildInput( InputBuilder inputBuilder );
+}
---
+++ code/Core/Character.Camera.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+
+namespace gm1.Core;
+
+public partial class Character
+{
+	private CameraComponent _cameraComponent;
+
+	public override void PostCameraSetup( ref CameraSetup camSetup )
+	{
+		base.PostCameraSetup( ref camSetup );
+
+		_cameraComponent ??= Components.Get<CameraComponent>();
+
+		_cameraComponent?.PostCameraSetup( ref camSetup );
+	}
+
+	public override void BuildInput( InputBuilder inputBuilder )
+	{
+		base.BuildInput( inputBuilder );
+
+		_cameraComponent ??= Components.Get<CameraComponent>();
+
+		_cameraComponent?.BuildInput( inputBuilder );
+	}
+}
