d38c5f4e86164af8edee9836274670eb8cc517c6
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-06 01:28:47 +0800

    overworld: `CameraLocation` impl

---
+++ code/Overworld/CameraLocation.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace gm1.Overworld;
+
+[Library( "gm1_cameralocation" ), HammerEntity]
+[Title( "Camera Location" ), Category( "Gameplay" ), Icon( "place" )]
+[Description( "Camera object to be used in events / trigger spots" )]
+[EditorModel( "models/editor/camera.vmdl" )]
+public partial class CameraLocation : Entity
+{
+	[Net]
+	[Property( Title = "Field of View" )]
+	public float FieldOfView { get; set; } = 80.0f;
+}
